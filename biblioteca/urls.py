421b24c5d8cd6fd3de481ac1cb7172164c5d5a10
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-26 18:18:49 -0300

    Adicinado url usuario

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('livro/', include('livro.urls')),
+    path('auth/', include('usuario.urls')),
 ]
