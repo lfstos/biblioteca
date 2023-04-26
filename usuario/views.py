6401f9c4738121f776d3182f3cce8e123a48fc75
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-26 18:20:05 -0300

    Criado função login, cadastro e valida_cadastro

--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from .models import User
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def login(request):
+    return HttpResponse('login')
+
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+
+def valida_cadastro(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    
+    user = User.objects.filter(email=email)
+    if user or email == '' or senha == '':
+        return render(request, 'cadastro.html', {'nome': nome})
+    else:
+        user = User()
+        user.first_name = nome
+        user.email = email
+        user.password = make_password(senha)
+        user.save()
+        return HttpResponse(f'Salvo com sucesso!')
