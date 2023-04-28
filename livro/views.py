023d32790b02826eeacdc1beeec4bfde496aea47
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-27 21:31:52 -0300

    Criada função home e adicionado login_required

--- livro/views.py
+++ livro/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 # Create your views here.
-def cadastrar(request):
-    return HttpResponse('Olá')
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
