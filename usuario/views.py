a16dc0be6cce6e5ca2d24c047957d6fd8d55b843
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-27 22:23:34 -0300

    Alterada funções login, valida_cadastro e valida_login

--- usuario/views.py
+++ usuario/views.py
@@ -1,31 +1,68 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, logout, login as login_django
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password
+from django.shortcuts import redirect, get_object_or_404
 from .models import User
 from django.http import HttpResponse
 
 # Create your views here.
 
 
+# @login_required(login_url='/auth/login/')
 def login(request):
-    return HttpResponse('login')
+    status = request.GET.get('status')
+    return render(request, 'login.html', {'status': status})
 
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    status = request.GET.get('status')
+    return render(request, 'cadastro.html', {'status': status})
 
 
 def valida_cadastro(request):
     nome = request.POST.get('nome')
     email = request.POST.get('email')
     senha = request.POST.get('senha')
-    
+
+    context = {
+        'nome': nome,
+        'email': email,
+    }
+
     user = User.objects.filter(email=email)
-    if user or email == '' or senha == '':
-        return render(request, 'cadastro.html', {'nome': nome})
-    else:
-        user = User()
-        user.first_name = nome
-        user.email = email
-        user.password = make_password(senha)
+
+    if user:
+        return redirect('/auth/cadastro/?status=1')
+
+    if nome.strip() == '' or email.strip() == '':
+        return redirect('/auth/cadastro/?status=2', context)
+
+    if len(senha) < 8:
+        return redirect('/auth/cadastro/?status=3', context)
+
+    try:
+        senha = make_password(senha)
+        user = User(first_name=nome, email=email, password=senha)
         user.save()
-        return HttpResponse(f'Salvo com sucesso!')
+        return redirect('/auth/cadastro/?status=0')
+    except:
+        return redirect('/auth/cadastro/?status=4')
+
+
+def valida_login(request):
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+
+    user = authenticate(request, email=email, password=senha)
+    print(f'user ==> {user}')
+    if user is not None:
+        login_django(request, user)
+        return redirect('home')
+    else:
+        return redirect('/auth/login/?status=1')
+
+
+def sair(request):
+    logout(request)
+    return redirect('/auth/login/')
