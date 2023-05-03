2f6248e95b64f7cfe67fc1705d28ae2bb7767933
Luiz Fernando <luiz-fernando@outlook.com>
2023-05-03 13:51:28 -0300

    Filtrando categorias pelo usuário e emprestimos pelo livro

--- livro/views.py
+++ livro/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Livros
+from .models import Livros, Categoria, Emprestimo
 
 
 @login_required
@@ -13,4 +13,16 @@ def home(request):
 @login_required
 def ver_livro(request, id):
     livro = Livros.objects.get(id=id)
-    return render(request, 'ver_livro.html', {'livro': livro})
+    usuario_id = request.user.id
+    categorias = Categoria.objects.filter(usuario=usuario_id)
+    emprestimos = Emprestimo.objects.filter(livro_id=id)
+    print(emprestimos)
+    context = {
+        'livro': livro,
+        'categorias': categorias,
+        'emprestimos': emprestimos
+    }
+    if request.user.id == livro.usuario.id:
+        return render(request, 'ver_livro.html', context)
+    else:
+        return redirect('home')
