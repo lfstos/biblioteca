5ab115ce77f680c8ec3983df43d8f8bb4413d397
Luiz Fernando <luiz-fernando@outlook.com>
2023-05-05 09:53:50 -0300

    Criada função cadastrar_livro e instanciado a classe CadastroLivroForm a função home

--- livro/views.py
+++ livro/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Livros, Categoria, Emprestimo
+from .forms import CadastroLivroForm
+from django.http import HttpResponse
 
 
 @login_required
 def home(request):
     id = request.user.id
     livros = Livros.objects.filter(usuario=id)
-    return render(request, 'home.html', {'livros': livros})
+    form = CadastroLivroForm()
+    
+    context = {
+        'livros': livros,
+        'form': form
+
+    }
+    return render(request, 'home.html', context)
 
 
 @login_required
@@ -16,7 +25,7 @@ def ver_livro(request, id):
     usuario_id = request.user.id
     categorias = Categoria.objects.filter(usuario=usuario_id)
     emprestimos = Emprestimo.objects.filter(livro_id=id)
-    print(emprestimos)
+    
     context = {
         'livro': livro,
         'categorias': categorias,
@@ -26,3 +35,14 @@ def ver_livro(request, id):
         return render(request, 'ver_livro.html', context)
     else:
         return redirect('home')
+
+
+@login_required
+def cadastrar_livro(request):
+    if request.method == 'POST':
+        form = CadastroLivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            return HttpResponse(form.errors)
