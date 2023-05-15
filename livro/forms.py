9c4c28b58809ac3aacd833da9c362d159a06b0b3
Luiz Fernando <luiz-fernando@outlook.com>
2023-05-15 20:42:33 -0300

    Criado classe CategoriaForm

--- livro/forms.py
+++ livro/forms.py
@@ -3,7 +3,7 @@ from .models import Livros, Categoria
 
 
 # class CadastroLivroForm(forms.ModelForm):
-class CadastroLivroForm(forms.ModelForm):
+class LivroForm(forms.ModelForm):
     class Meta:
         model = Livros
         fields = '__all__'
@@ -20,3 +20,15 @@ class CadastroLivroForm(forms.ModelForm):
         self.fields['usuario'].initial = request.user.id
         self.fields['usuario'].widget = forms.HiddenInput()
         self.fields['categoria'].queryset = Categoria.objects.filter(usuario=request.user.id)
+
+
+class CategoriaForm(forms.Form):
+    nome = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    descricao = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    # usuario = forms.ChoiceField()1
+    
+    
+    
+    # nome = models.CharField(max_length=30)
+    # descricao = models.TextField()
+    # usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
