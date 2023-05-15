65981f1843d928db20883751f4f79aefcd8f6f17
Luiz Fernando <luiz-fernando@outlook.com>
2023-05-15 20:36:10 -0300

    Configurado bd Postgres

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'biblioteca',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '', # 8000 is default
     }
 }
 
