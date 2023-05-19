a230e25eb607a92f93dc3d643b2ea23b63021679
Luiz Fernando <luiz-fernando@outlook.com>
2023-05-19 10:35:57 -0300

    Criada urls, cadastrar_livro, cadastrar_emprestimo e devolver_livro

--- livro/urls.py
+++ livro/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('ver_livro/<int:id>', views.ver_livro, name='ver_livro'),
-    path('cadastrar_livro', views.cadastrar_livro, name='cadastrar_livro'),
+    path('cadastrar_livro/', views.cadastrar_livro, name='cadastrar_livro'),
     path('excluir_livro/<int:id>', views.excluir_livro, name='excluir_livro'),
     path('cadastrar_categoria/', views.cadastrar_categoria, name='cadastrar_categoria'),
-    path('cadastrar_emprestimo', views.cadastrar_emprestimo, name='cadastrar_emprestimo'),
+    path('cadastrar_emprestimo/', views.cadastrar_emprestimo, name='cadastrar_emprestimo'),
+    path('devolver_livro/', views.devolver_livro, name='devolver_livro')
 ]
